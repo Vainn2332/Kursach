59d168d24e606503837d3ab0ccb98f2a99cfe2de
Владислав <vladuk2332@gmail.com>
2025-09-27 18:07:13 +0300

    added background gradient

--- Kursach/Form1.cs
+++ Kursach/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,25 @@ namespace Kursach
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {//162, 197, 35 лайм
+         
+            FormGradientPainter fgp = new FormGradientPainter();
+            Color color1 = Color.FromArgb(146, 99, 165);
+            Color color2 = Color.FromArgb(125, 159, 207);
+            Color color3 = Color.FromArgb(196, 231, 242);
+
+            fgp.CreateLinearGradient(this, color1, color2, color3, e);
+        }
+
+
+        
     }
 }
+
---
+++ Kursach/FormGradientPainter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    internal class FormGradientPainter
+    {
+        public void CreateLinearGradient(Form form,Color color1,Color color2,Color color3,PaintEventArgs e)
+        {//заливает градиентом данную форму 3 цветами от верхнего левого угла до правого нижнего
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(form.Width,form.Height);
+            Color[] colors = new Color[]
+            {
+                 color1,
+                 Color.FromArgb((color1.R+color2.R)/2,(color1.G+color2.G)/2,(color1.B+color2.B)/2),
+                 color2,
+                 Color.FromArgb((color2.R+color3.R)/2,(color2.G+color3.G)/2,(color2.B+color3.B)/2),   // Промежуточные  
+                 color3
+            };
+            Rectangle rect = form.ClientRectangle;
+
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush(rect, colors[0], colors[4], LinearGradientMode.ForwardDiagonal);
+            ColorBlend blend = new ColorBlend();
+            blend.Colors = colors;
+            blend.Positions = new float[] { 0f, 0.2f, 0.5f, 0.92f, 1f };
+            linearGradientBrush.InterpolationColors = blend;
+            e.Graphics.FillRectangle(linearGradientBrush, rect);
+        }
+    }
+}
