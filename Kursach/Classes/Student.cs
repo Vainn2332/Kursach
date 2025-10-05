161864bbfec7a97b67bb114d06079226d8895e69
Владислав <vladuk2332@gmail.com>
2025-10-05 20:12:14 +0300

    added student class

--- Kursach/Classes/Student.cs
+++ Kursach/Classes/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +17,21 @@ namespace Kursach
         public int Course{get;private set;}
         public string Specialisation{get;private set;}
         public string Descryption { get; set; }//описание студента
+        public Student(int ID,string firstName,string lastName,string surName,int age,int course,string specialisation,string descryption)
+        {
+            this.ID = ID;
+            FirstName = firstName;
+            LastName = lastName;
+            SurName = surName;
+            Age = age;
+            Course = course;
+            Specialisation = specialisation;
+            Descryption = descryption;
+        }
         public string GetData()
         {
             return $"{ID}\t{FirstName}\t{LastName}\t{SurName}\t{Age}\t{Course}\t{Specialisation}";
         }
+
     }
 }
